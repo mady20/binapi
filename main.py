ed438cdfdb587a0dbd883fe87275739953d99688
mady20 <165700212+mady20@users.noreply.github.com>
2025-09-12 21:31:58 +0530

    crud without db done!

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import secrets
 from fastapi import FastAPI, HTTPException
 from typing import Any
 
-
 app = FastAPI(root_path="/api/v1")
 existing_ids: set[str] = set()
 
@@ -27,15 +26,8 @@ data : list[dict[str,Any]]  =  [
             "content" : "sample paste 2 data"
         }
     ]
-
-
-@app.get("/archive")
-async def read_pastes():
-    return {
-        "pastes" : data 
-    }
-
-@app.get("/{id}")
+    
+@app.api_route("/pastes/{id}", methods=["GET", "HEAD"])
 async def read_paste(id: str):
     for paste in data:
         if paste.get("id") == id:
@@ -45,8 +37,18 @@ async def read_paste(id: str):
         
     raise HTTPException(status_code=404, detail="Paste not found")
 
+@app.delete("/pastes/{id}")
+async def delete_paste(id: str):
+    for i in range(len(data)):
+        paste = data[i]
+        if paste.get("id") == id:
+            data.pop(i)
+            return {
+                "message": "Paste successfully deleted!"
+            }
+    raise HTTPException(status_code=404, detail="Paste not found")
 
-@app.post("/")
+@app.post("/pastes")
 async def create_paste(body: dict[str, Any]):
     paste : Any = {
         "id" : generate_unique_id(),
@@ -57,4 +59,27 @@ async def create_paste(body: dict[str, Any]):
     data.append(paste)  
     return {    
         "paste": paste
+    }
+
+@app.put("/pastes/{id}")
+async def update_paste(id: str, body: dict[str, Any]):
+    for i in range(len(data)):
+        paste = data[i]
+        if paste.get("id") == id:
+            newPaste : Any = {
+                "id" : id, 
+                "name" : body.get("name") ,
+                "expiration" : body.get("expiration"),
+                "content" : body.get("content")
+            }
+            data[i] = newPaste
+            return {
+                    "paste" : newPaste
+            }
+    raise HTTPException(status_code=404, detail="Paste Not Found!")
+
+@app.api_route("/pastes", methods=["GET", "HEAD"])
+async def read_pastes():
+    return {
+        "pastes" : data 
     }
